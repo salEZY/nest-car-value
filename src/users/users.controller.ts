a3228717d578d4c15f86328af9b24ee1bd66b896
salEZY <bobicaleks@yahoo.com>
2022-03-03 13:18:53 +0100

    add error handling with NotFoundException

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -7,6 +7,7 @@ import {
   Body,
   Param,
   Query,
+  NotFoundException,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user-dto';
@@ -22,12 +23,14 @@ export class UsersController {
   }
 
   @Get('/:id')
-  findUser(@Param('id') id: string) {
-    return this.usersService.findOne(parseInt(id));
+  async findUser(@Param('id') id: string) {
+    let user = await this.usersService.findOne(parseInt(id));
+    if (!user) throw new NotFoundException('user not found');
+    return user;
   }
 
   @Get('/')
-  findAllUsers(@Query('email') email: string) {
+  async findAllUsers(@Query('email') email: string) {
     return this.usersService.find(email);
   }
 
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
@@ -22,7 +22,7 @@ export class UsersService {
 
   async update(id: number, attrs: Partial<User>) {
     const user = await this.findOne(id);
-    if (!user) throw new Error('user not found');
+    if (!user) throw new NotFoundException('user not found');
 
     Object.assign(user, attrs);
 
@@ -31,7 +31,7 @@ export class UsersService {
 
   async remove(id: number) {
     const user = await this.findOne(id);
-    if (!user) throw new Error('user not found');
+    if (!user) throw new NotFoundException('user not found');
 
     return this.repo.remove(user);
   }
