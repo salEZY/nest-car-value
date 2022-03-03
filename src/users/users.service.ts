425493a771c48a435c10486f34de4e59a7cf5cca
salEZY <bobicaleks@yahoo.com>
2022-03-03 10:47:20 +0100

    add update and remove in users.service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -20,7 +20,19 @@ export class UsersService {
     return this.repo.find({ email });
   }
 
-  update(id: number) {}
+  async update(id: number, attrs: Partial<User>) {
+    const user = await this.findOne(id);
+    if (!user) throw new Error('user not found');
 
-  remove(id: number) {}
+    Object.assign(user, attrs);
+
+    return this.repo.save(user);
+  }
+
+  async remove(id: number) {
+    const user = await this.findOne(id);
+    if (!user) throw new Error('user not found');
+
+    return this.repo.remove(user);
+  }
 }
