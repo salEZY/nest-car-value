49404813f956ba733ab75bd506d7be9fab99c00f
salEZY <bobicaleks@yahoo.com>
2022-03-03 13:01:41 +0100

    implement update to users controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -9,6 +9,7 @@ import {
   Query,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { UpdateUserDto } from './dtos/update-user-dto';
 import { UsersService } from './users.service';
 
 @Controller('auth')
@@ -28,6 +29,11 @@ export class UsersController {
   @Get('/')
   findAllUsers(@Query('email') email: string) {
     return this.usersService.find(email);
+  }
+
+  @Patch('/:id')
+  updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+    return this.usersService.update(parseInt(id), body);
   }
 
   @Delete('/:id')
