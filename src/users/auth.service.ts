d0f909b5cede5c98b659f72e96842d5d93e37067
salEZY <bobicaleks@yahoo.com>
2022-03-05 09:55:22 +0100

    hash password in signup

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -1,6 +1,9 @@
 import { BadRequestException, Injectable } from "@nestjs/common";
 import { UsersService } from "./users.service";
+import { randomBytes, scrypt as _scrypt } from "crypto";
+import { promisify } from "util";
 
+const scrypt = promisify(_scrypt)
 
 @Injectable()
 export class AuthService {
@@ -10,8 +13,13 @@ export class AuthService {
         // Email in use?
         const users = await this.usersService.find(email)
         if (users.length) throw new BadRequestException('Email already in use.')
-        // Hash password
-
+        // Hash password:
+        // 1.Salt
+        const salt = randomBytes(8).toString('hex')
+        // 2.Hash salt and password
+        const hash = (await scrypt(password, salt, 32)) as Buffer
+        // 3.Join the hashed result and salt together
+        const result = `${salt}.${hash.toString('hex')}`
         // Create a new user and save it
 
         // Return user
