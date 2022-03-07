031243fb49e5832ce59109b8d1aeca58dd6104d5
salEZY <bobicaleks@yahoo.com>
2022-03-07 15:56:44 +0100

    create current-user interceptor and decorator, refactor

--- src/users/decorators/current-user.decorator.ts
+++ src/users/decorators/current-user.decorator.ts
@@ -1,7 +1,8 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 
 export const CurrentUser = createParamDecorator(
-  (data: any, context: ExecutionContext) => {
-    return 'hi there';
+  (data: never, context: ExecutionContext) => {
+    const request = context.switchToHttp().getRequest();
+    return request.currentUser;
   },
 );
