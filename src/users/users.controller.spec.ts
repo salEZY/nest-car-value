23dcae77cb387be94e0f501c7b422c989f4ca77d
salEZY <bobicaleks@yahoo.com>
2022-03-16 16:19:22 +0100

    add tests for users.controller

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -11,19 +11,31 @@ describe('UsersController', () => {
 
   beforeEach(async () => {
     fakeUsersService = {
-      findOne: () => {},
-      find: () => {},
-      remove: () => {},
-      update: () => {},
+      findOne: (id: number) => {
+        return Promise.resolve({
+          id,
+          email: 'test@test.com',
+          password: 'asdf',
+        } as User);
+      },
+      find: (email: string) => {
+        return Promise.resolve([{ id: 1, email, password: 'asdf' } as User]);
+      },
+      /*  remove: (id: number) => {},
+      update: () => {}, */
     };
 
     fakeAuthService = {
-      signup: () => {},
-      signin: () => {},
+      /* signup: () => {},
+      signin: () => {}, */
     };
 
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
+      providers: [
+        { provide: UsersService, useValue: fakeUsersService },
+        { provide: AuthService, useValue: fakeAuthService },
+      ],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
@@ -31,5 +43,22 @@ describe('UsersController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('find all users with same email', async () => {
+    const users = await controller.findAllUsers('test@test.com');
+    expect(users.length).toEqual(1);
+    expect(users[0].email).toEqual('test@test.com');
+  });
+
+  it('find user with given id', async () => {
+    const user = await controller.findUser('2');
+
+    expect(user).toBeDefined();
+  });
+
+  it('find user - throw error', (done) => {
+    fakeUsersService.findOne = () => null;
+    controller.findUser('1').catch(() => done());
   });
 });
