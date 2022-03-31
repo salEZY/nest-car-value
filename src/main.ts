838ee1816fc1b97f11cc5d394233eafb1ef0cdd5
salEZY <bobicaleks@yahoo.com>
2022-03-31 11:07:38 +0200

    start on e2e tests, refactor app.module and main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,10 @@
 import { NestFactory } from '@nestjs/core';
-import { ValidationPipe } from '@nestjs/common';
-const cookieSession = require('cookie-session');
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(
-    cookieSession({
-      keys: ['cookies'],
-    }),
-  );
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-    }),
-  );
+
   await app.listen(3000);
 }
 bootstrap();
