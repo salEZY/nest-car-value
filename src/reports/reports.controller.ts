0c8bbfe2233939abfd6f154e9659c945c1139243
salEZY <bobicaleks@yahoo.com>
2022-06-15 19:06:24 +0200

    start on get estimate car value route

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -5,6 +5,8 @@ import {
   UseGuards,
   Patch,
   Param,
+  Get,
+  Query
 } from '@nestjs/common';
 import { CreateReportDto } from './dtos/create-report.dto';
 import { ReportsService } from './reports.service';
@@ -15,10 +17,16 @@ import { ReportDto } from './dtos/report.dto';
 import { Serialize } from '../interceptors/serialize.interceptor';
 import { ApproveReportDto } from './dtos/approve-report.dto';
 import { AdminGuard } from '../guards/admin.guard';
+import { GetEstimateDto } from './dtos/get-estimate.dto';
 
 @Controller('reports')
 export class ReportsController {
   constructor(private reportsService: ReportsService) { }
+
+  @Get()
+  getEstimate(@Query() query: GetEstimateDto) {
+
+  }
 
   @Post()
   @UseGuards(AuthGuard)
@@ -33,4 +41,6 @@ export class ReportsController {
   approveReport(@Param('id') id: string, @Body() body: ApproveReportDto) {
     return this.reportsService.changeApproval(id, body.approved);
   }
+
+
 }
