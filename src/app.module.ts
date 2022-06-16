d337a9c763349ca3310d74127cbcb9cb17ec2ff8
salEZY <bobicaleks@yahoo.com>
2022-06-16 20:50:06 +0200

    start deploy, set cookie key env

--- src/app.module.ts
+++ src/app.module.ts
@@ -47,12 +47,14 @@ const cookieSession = require('cookie-session');
     },
   ],
 })
+
 export class AppModule {
+  constructor(private configService: ConfigService) { }
   configure(consumer: MiddlewareConsumer) {
     consumer
       .apply(
         cookieSession({
-          keys: ['cookies'],
+          keys: [this.configService.get('COOKIE_KEY')],
         }),
       )
       .forRoutes('*');
