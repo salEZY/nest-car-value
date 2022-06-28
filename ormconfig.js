f70e4a4878f2659b0119f9ad8e61c1745522002a
salEZY <bobicaleks@yahoo.com>
2022-06-28 11:34:47 +0200

    start work on migration

---
+++ ormconfig.js
@@ -0,0 +1,25 @@
+const dbConfig = {
+    synchronize: false
+}
+
+switch (process.env.NODE_ENV) {
+    case 'development':
+        Object.assign(dbConfig, {
+            type: 'sqlite',
+            database: 'db.sqlite',
+            entities: ['**/*.entity.js']
+        })
+        break
+    case 'test':
+        Object.assign(dbConfig, {
+            type: 'sqlite',
+            database: 'test.sqlite',
+            entities: ['**/*.entity.ts']
+        })
+        break
+    case 'production':
+    default:
+        throw new Error('Unknown enviroment.')
+}
+
+module.exports = dbConfig
